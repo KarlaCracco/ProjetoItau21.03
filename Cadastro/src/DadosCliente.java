5557e1d2db780f9d2ec04b6fa9572e3dfca7720e
Karla Rojas Cracco Lima Império Dalmati <karla_cracco@hotmail.com>
2022-03-21 12:26:46 -0300

    ProjetoItáu21.03

---
+++ Cadastro/src/DadosCliente.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class DadosCliente {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			
+		
+		
+		
+		/*String nome,email,telefone;
+		int saldo,agencia,conta;
+		
+		Scanner teclado = new Scanner(System.in);
+		
+		System.out.println("Digite o nome da cliente");
+		nome=teclado.nextLine();
+		
+		System.out.println("Entre com seu o email");*/
+		
+		
+
+	}
+
+}
